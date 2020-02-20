4e67dbe68646acb440bf824784338f8613e52d64
Zack Claar <claar.zack@gmail.com>
2020-02-20 03:26:56 -0700

    added click event for selecting a hero, logic for showing div when user clicks on hero, added toggle for css style class with class binding

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 // Always import the component symbol from the angular core library
 import { Component, OnInit } from '@angular/core';
 import { Hero } from "../hero";
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -11,17 +12,29 @@ import { Hero } from "../hero";
 // Always export the class so you can import it elsewhere
 export class HeroesComponent implements OnInit {
 
+  // Defining a component property to expose the HEROES array we are importing
+  // but will eventually get from a database
+  heroes = HEROES;
+  selectedHero: Hero;
+
   // We now changed the previously stored string to an object whose interface is exported
-  hero: Hero = {
-    id: 1,
-    name: "yR"
-  }; 
+  // hero: Hero = {
+    // id: 1,
+    // name: "yR"
+  // }; 
+
 
   constructor() { }
 
   // Anfular calls on the init function shortly after creating a component
   // and this is a good place to put any initialization logic
   ngOnInit(): void {
+  }
+
+  // This triggers when the onclick event is fired on one of the list elements
+  // and sets the selectedHero to whichever li was clicked on
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+// Eventually we will get this list from a database somewhere
+// but for now we can just make an array and import it. 
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: '123' },
+    { id: 12, name: 'qwe' },
+    { id: 13, name: 'asd' },
+    { id: 14, name: 'zxc' },
+    { id: 15, name: '456' },
+    { id: 16, name: 'rty' },
+    { id: 17, name: 'fgh' },
+    { id: 18, name: 'vbn' },
+    { id: 19, name: '789' },
+    { id: 20, name: 'uio' } 
+];
